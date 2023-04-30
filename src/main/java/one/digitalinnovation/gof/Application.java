e249b2fcadbc01e68626663a750d66a454c40c46
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-04-30 20:47:33 -0300

    final commit

---
+++ src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+
